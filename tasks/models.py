baaafc37cae93bfa43f56ee134a26ae021dd4725
Fija-khan <fijapathan9763@gmail.com>
2026-06-19 17:18:18 +0530

    Add: Implement Task model with assignment and tracking features — tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
+from contacts.models import Contact
+from leads.models import Lead
 
-# Create your models here.
+class Task(models.Model):
+    
+    PRIORITY_CHOICES = (
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+        ('urgent', 'Urgent'),
+    )
+    
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('in_progress','In Progress'),
+        ('done', 'Done'),
+    )
+    
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    assigned_to = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    related_contact = models.ForeignKey(Contact, null=True, blank=True, on_delete=models.SET_NULL )
+    related_lead = models.ForeignKey(Lead, null=True, blank=True, on_delete=models.SET_NULL)
+    due_date = models.DateField(null=True, blank=True)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
