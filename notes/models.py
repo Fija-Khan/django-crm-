3bf8fc73d3d7e740a94823732d1155ec59077f3d
Fija-khan <fijapathan9763@gmail.com>
2026-06-21 19:43:26 +0530

    Add: Note model and admin registration - notes app

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+from contacts.models import Contact
+from leads.models import Lead
 
-# Create your models here.
+class Note(models.Model):
+    
+    content = models.TextField()
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, null=True, blank=True)
+    lead = models.ForeignKey(Lead, on_delete=models.CASCADE, null=True, blank=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.content[:50]
+
+    
